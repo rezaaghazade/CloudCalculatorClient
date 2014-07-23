48bafa301e23a82a5bd5a0de5309d73c234c0c95
reza <reza.zah1991@gmail.com>
2014-07-23 14:53:54 +0430

    Initial commit

---
+++ src/org/cloud/client/Main.java
@@ -0,0 +1,17 @@
+package org.cloud.client;
+
+import org.j2os.shine.util.reflect.Application;
+import org.j2os.shine.util.reflect.ServerApplication;
+
+/**
+ * Created by reza on 7/23/14.
+ */
+public class Main {
+    public static void main(String[] args)throws Exception{
+
+
+
+
+
+    }
+}
---
+++ src/org/cloud/client/ServerConn.java
@@ -0,0 +1,16 @@
+package org.cloud.client;
+
+import org.j2os.shine.util.reflect.ServerApplication;
+
+/**
+ * Created by reza on 7/23/14.
+ */
+public class ServerConn {
+
+    public static ServerApplication serverApplication=null;
+
+    public ServerApplication getServerApplication()
+    {
+        return serverApplication;
+    }
+}
